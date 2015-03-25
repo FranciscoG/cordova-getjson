604de22d77715821717030670b69f6b877feb317
franciscog <frankjg@gmail.com>
2015-03-25 08:12:19 -0400

    added error callback

--- www/CordovaGetJSON.js
+++ www/CordovaGetJSON.js
@@ -9,9 +9,9 @@ function CordovaGetJSON() {}
  * @param {String} api                The Preference name in your config.xml that contains the value you want
  * @param {Function} successCallback  The function to call when the heading data is available
  */
-CordovaGetJSON.prototype.get = function (api, successCallback) {
+CordovaGetJSON.prototype.get = function (api, successCallback, errorCallback) {
     argscheck.checkArgs('sF', 'CordovaGetJSON.getInfo');
-    exec(successCallback, errorCallback, "CordovaGetJSON", "startAPIcalls", [api]);
+    exec(successCallback, errorCallback, "CordovaGetJSON", "get", [api]);
 };
 
 module.exports = new CordovaGetJSON();
